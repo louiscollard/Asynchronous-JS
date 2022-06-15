91e8b6d3e17353a16ed3d5746dd55f98f577f688
Louis Collard <louiscollard2@gmail.com>
2022-06-15 15:33:25 +0200

    Not finished exo02

--- exercise_2/script.js
+++ exercise_2/script.js
@@ -1,8 +1,18 @@
 const fetchName = (name) => fetch("https://api.agify.io?name=" + name);
+const input = document.getElementById("name");
+const btn = document.querySelector("#button");
 
-fetchName("michael")
+fetchName("louis")
 	.then((res) => res.json())
-	.then((data) => console.log(data))
+	.then((data) => {
+		btn.addEventListener("click", () => {
+			console.log(input);
+			const body = document.querySelector("body");
+			const div = document.createElement("div");
+			body.append(div);
+			div.textContent = data;
+		});
+	})
 	.catch((error) => {
 		console.log("There was an error", error);
 	});
