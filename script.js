497c0c4f65f8f41d0b0bca83d35954edeccb5512
Louis Collard <louiscollard2@gmail.com>
2022-06-15 13:57:56 +0200

    Finish exercise 01

---
+++ script.js
@@ -0,0 +1,17 @@
+// const fetchName = (name) => fetch("becode.json");
+const btn = document.querySelector("button");
+
+fetch("becode.json")
+	.then((response) => response.json())
+	.then((data) => {
+		btn.addEventListener("click", () => {
+			const body = document.querySelector("body");
+			const ul = document.createElement("ul");
+			body.appendChild(ul);
+			for (let rules of data) {
+				let li = document.createElement("li");
+				ul.appendChild(li);
+				li.textContent = rules;
+			}
+		});
+	});
